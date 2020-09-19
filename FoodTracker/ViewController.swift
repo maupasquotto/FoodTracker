1f08db07acb7530e5467ece952d1ef2650d87b51
Mauricio Pasquotto <maupasquotto@gmail.com>
2020-09-19 14:52:02 -0300

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by mauricio.pasquotto on 19/09/20.
+//  Copyright © 2020 Mauricio Pasquotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
