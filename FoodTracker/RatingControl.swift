c103d25be58b04d6f773c04281331e0a1fda4073
Mauricio Pasquotto <maupasquotto@gmail.com>
2020-09-24 20:57:30 -0300

    Stating rating stars

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by mauricio.pasquotto on 24/09/20.
+//  Copyright © 2020 Mauricio Pasquotto. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
