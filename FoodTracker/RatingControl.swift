223b7c8164f6a9981dc953f1c95925c61b9d99a0
Mauricio Pasquotto <maupasquotto@gmail.com>
2020-09-24 22:56:25 -0300

    WIP Meal Rating

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,10 +13,32 @@ class RatingControl: UIStackView {
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stackView
+        addArrangedSubview(button)
+    }
+    
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
     }
     
 }
