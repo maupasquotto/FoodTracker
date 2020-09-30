9bc1103ad0297edfbead13d3b442291490a3e349
Mauricio Pasquotto <maupasquotto@gmail.com>
2020-09-29 21:33:05 -0300

    Meals list WIP

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by mauricio.pasquotto on 29/09/20.
+//  Copyright © 2020 Mauricio Pasquotto. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
