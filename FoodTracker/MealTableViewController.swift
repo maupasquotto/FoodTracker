021f5200f0c65de45d78871fafd847c86239578b
Mauricio Pasquotto <maupasquotto@gmail.com>
2020-10-10 00:02:52 -0300

    Editing doesn't show warning but does not do animation

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -126,15 +126,18 @@ class MealTableViewController: UITableViewController {
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 // Update an existing meal.
                 meals[selectedIndexPath.row] = meal
-                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+                //tableView.reloadRows(at: [selectedIndexPath], with: .none)
             }
             else {
                 // Add a new meal.
                 let newIndexPath = IndexPath(row: meals.count, section: 0)
                 
                 meals.append(meal)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
+                //tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            
+            //TODO: The tableView methods above are throwing a weird warning
+            tableView.reloadData()
         }
     }
     
