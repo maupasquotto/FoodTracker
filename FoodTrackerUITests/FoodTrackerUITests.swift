97386a4a26439e6be5b920004d1632a4075a0eb3
Mauricio Pasquotto <maupasquotto@gmail.com>
2020-10-11 18:27:39 -0300

    UI Test for editing WIP

--- FoodTrackerUITests/FoodTrackerUITests.swift
+++ FoodTrackerUITests/FoodTrackerUITests.swift
@@ -62,7 +62,40 @@ class FoodTrackerUITests: XCTestCase {
         // Launch App
         app.launch()
         
+        // Get total meals
+        let mealCountInitial = app.tables.staticTexts.count
+        
         // Tap first meal
-        app.tables.cells.element(boundBy: 0)
+        let firstMeal = app.tables.cells.element(boundBy: 0)
+        tapCoordinate(at: Double(firstMeal.frame.origin.x), and: Double(firstMeal.frame.origin.y))
+        
+        // meal name type
+        let mealName = app.textFields["mealName"]
+        mealName.tap()
+        mealName.typeText("Teste 1")
+        
+        // meal photo select
+        let image = app.images["mealPhoto"]
+        image.tap()
+        app.collectionViews()[0].visibleCells()[0].tap();
+        //app.tables.cells.element(boundBy: 0).tap()
+        
+        // rating selection
+        let mealRating = app.otherElements["mealRating"]
+        mealRating.buttons.element(boundBy: 1).tap()
+        
+        // save
+        app.buttons["mealSave"].tap()
+        
+        // check if it was created
+        let mealCountNow = app.tables.staticTexts.count
+        XCTAssertTrue(mealCountNow > mealCountInitial)
+        
+    }
+    
+    func tapCoordinate(at xCoordinate: Double, and yCoordinate: Double) {
+        let normalized = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
+        let coordinate = normalized.withOffset(CGVector(dx: xCoordinate, dy: yCoordinate))
+        coordinate.tap()
     }
 }
